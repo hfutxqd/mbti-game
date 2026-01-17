45a4ba55ba6b8364b592d18c37e2cceb6997fd04
fishand <ahtlxqd@gmail.com>
2026-01-17 22:16:31 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const base = env.VITE_BASE || "/";
+
+  return {
+    base,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
